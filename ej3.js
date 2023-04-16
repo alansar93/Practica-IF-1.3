1c09d0f72bb8470c5ff72a1cc6b053def5424af6
Alan Sarchman <alansarchman@live.com.ar>
2023-04-16 11:49:47 -0300

    actualizado y mejorado

--- ej3.js
+++ ej3.js
@@ -2,27 +2,18 @@ const PRIMER_PREMIO = "1000000 USD!!!!";
 const SEGUNDO_PREMIO = "500000 USD!!";
 const TERCER_PREMIO = "Un Helado de limon =(";
 
-let numeroElegir = prompt("Elegi un numero del 1 al 5");
-let numeroUno = "1";
-let numeroDos = "2";
-let numeroTres = "3";
+let numeroUsuarioPrimero = prompt("Introducir el primer numero obtenido");
+let numeroUsuarioSegundo = prompt("Introducir el segundo numero obtenido");
+let numeroUsuarioTercero = prompt("Introducir el tercer numero obtenido");
 
-if ((numeroElegir >= 1) && (numeroElegir <= 5)) {
-    if (numeroElegir == numeroUno) {
-        document.write ("GANASTE " + PRIMER_PREMIO + " FELICITACIONES CAMPEON")
-    } else if (numeroElegir == numeroDos) {
-        document.write ("FELICITACIONES GANASTE " + SEGUNDO_PREMIO)
-            }else if (numeroElegir == numeroTres) {
-                document.write ("POR LO MENOS TE LLEVAS" + TERCER_PREMIO + " MAS SUERTE PARA LA PROXIMA")
-            } else if ((numeroElegir = 4) || (numeroElegir = 5))  {
-                document.write ("NO GANASTE NADA MAS SUERTE PARA LA PROXUMA")
-            }
-} else if ((numeroElegir <= 0) || (numeroElegir >= 6)){
-    document.write ("Game Over")
-    
+
+if ((numeroUsuarioPrimero == numeroUsuarioSegundo) && (numeroUsuarioPrimero == numeroUsuarioTercero)) {
+    document.write("FELICIDADES GANASTE " + PRIMER_PREMIO + " SOS EL MEJOR");
+} else if ((numeroUsuarioPrimero == numeroUsuarioSegundo) || (numeroUsuarioPrimero == numeroUsuarioTercero) || (numeroUsuarioSegundo == numeroUsuarioTercero)) {
+    document.write("FELICIDADES GANSTE " + SEGUNDO_PREMIO + " MAS SUERTE PARA LA PROXIMA");
+} if ((numeroUsuarioPrimero !== numeroUsuarioSegundo) && (numeroUsuarioPrimero !== numeroUsuarioTercero) && (numeroUsuarioSegundo !== numeroUsuarioTercero)) {
+    document.write("Premio consueldo de " + TERCER_PREMIO)
 }
-
-
 
 
 
