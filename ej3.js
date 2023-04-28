eee7f5c59bc17a48adff79fdf10ea72682c9de67
Alan  Sarchman <alansarhchman@live.com.argit config --global user.emailgit config --globalgit config --global config user.namegit initgit config --global user.email alansarhchman@live.com.ar>
2023-04-28 20:47:35 -0300

    actualizado y correcto

--- ej3.js
+++ ej3.js
@@ -1,19 +1,37 @@
-const PRIMER_PREMIO = "1000000 USD!!!!";
-const SEGUNDO_PREMIO = "500000 USD!!";
-const TERCER_PREMIO = "Un Helado de limon =(";
 
-let numeroUsuarioPrimero = prompt("Introducir el primer numero obtenido");
+/* let numeroUsuarioPrimero = prompt("Introducir el primer numero obtenido");
 let numeroUsuarioSegundo = prompt("Introducir el segundo numero obtenido");
-let numeroUsuarioTercero = prompt("Introducir el tercer numero obtenido");
+let numeroUsuarioTercero = prompt("Introducir el tercer numero obtenido"); */
 
 
-if ((numeroUsuarioPrimero == numeroUsuarioSegundo) && (numeroUsuarioPrimero == numeroUsuarioTercero)) {
+/* if ((numeroUsuarioPrimero == numeroUsuarioSegundo) && (numeroUsuarioPrimero == numeroUsuarioTercero)) {
     document.write("FELICIDADES GANASTE " + PRIMER_PREMIO + " SOS EL MEJOR");
 } else if ((numeroUsuarioPrimero == numeroUsuarioSegundo) || (numeroUsuarioPrimero == numeroUsuarioTercero) || (numeroUsuarioSegundo == numeroUsuarioTercero)) {
     document.write("FELICIDADES GANSTE " + SEGUNDO_PREMIO + " MAS SUERTE PARA LA PROXIMA");
 } if ((numeroUsuarioPrimero !== numeroUsuarioSegundo) && (numeroUsuarioPrimero !== numeroUsuarioTercero) && (numeroUsuarioSegundo !== numeroUsuarioTercero)) {
     document.write("Premio consueldo de " + TERCER_PREMIO)
 }
+*/
+/* 
+const PRIMER_PREMIO = "1000000 USD!!!!";
+const SEGUNDO_PREMIO = "500000 USD!!";
+const TERCER_PREMIO = "Un Helado de limon" */
 
 
+const numero1 = document.querySelector("#numero1");
+const numero2 = document.querySelector("#numero2");
+const numero3 = document.querySelector("#numero3");
+const premioBingo = document.querySelector("#premio")
 
+function bingo() {
+ let numeroa = numero1.value
+let numerob = numero2.value
+let numeroc = numero3.value
+
+if ((numeroa == numerob) && (numerob == numeroc)) {
+    premioBingo.innerHTML = "GANASTE 1.000.000 USD!!!!!!!"
+} else if ((numeroa == numerob) || (numeroa == numeroc) || (numerob == numeroc)) {
+    premioBingo.innerHTML = "GANASTE 500.000 USD!!!!!"
+} else { premioBingo.innerHTML = "ganaste un helado de limon"
+} 
+}
